c5b0cc0cafef6dbd62f099d5884039a7c6985fdb
BassaniDavid <davbassan98@gmail.com>
2025-04-14 14:32:50 +0200

    feat: form created

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,67 @@ import { useState } from 'react'
 
 function App() {
 
+  const [formData, setFormData] = useState({
+    author: "",
+    title: "",
+    body: "",
+    public: true
+  })
+
+  function handleFormData(event) {
+    let value = event.target.type === "checkbox" ? event.target.checked : event.target.value
+
+    setFormData((formData) => ({
+      ...formData,
+      [event.target.name]: value,
+    }))
+  }
+
+  function sendData(event) {
+    event.preventDefault()
+    console.log(formData)
+  }
 
   return (
     <>
+      <form action="" onSubmit={sendData}>
+        <input
+          type="text"
+          name="author"
+          value={formData.author}
+          onChange={handleFormData}
+          placeholder="inserisci il nome dell'autore" />
+        <br />
 
+        <input
+          type="text"
+          name="title"
+          value={formData.title}
+          onChange={handleFormData}
+          placeholder="inserisci il nome dell'autore" />
+        <br />
+
+        <input
+          type="text"
+          name="body"
+          value={formData.body}
+          onChange={handleFormData}
+          placeholder="inserisci il nome dell'autore" />
+        <br />
+
+        <label htmlFor="public">disponibile</label>
+        <input
+          type="checkbox"
+          name="public"
+          checked={formData.available}
+          onChange={handleFormData} />
+        <br />
+
+        <button type="submit">invio form</button>
+      </form>
     </>
+
+
   )
 }
 
